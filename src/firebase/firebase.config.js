59e51c78049899b651a6f22ba59c2241c76289d4
tanvirfsd <tahasanpro@gmail.com>
2023-10-20 05:00:50 +0600

    updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,14 +4,16 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const all = import.meta.env
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDjXUqB2dDEWG2H3ZXssQQfo2nxXZ8C2xM",
-  authDomain: "eventist-48f83.firebaseapp.com",
-  projectId: "eventist-48f83",
-  storageBucket: "eventist-48f83.appspot.com",
-  messagingSenderId: "359291178485",
-  appId: "1:359291178485:web:f3c598b7e0e74895f664bc"
+  apiKey: all.VITE_apiKey ,
+  authDomain: all.VITE_authDomain,
+  projectId: all.VITE_projectId ,
+  storageBucket: all.VITE_storageBucket,
+  messagingSenderId: all.VITE_messagingSenderId,
+  appId: all.VITE_ppId,
 };
 
 // Initialize Firebase
